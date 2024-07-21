2cabec884701a7be7dae8f4f167001aec0ec124a
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-07-21 22:38:17 +0530

    Add files via upload

---
+++ Dsize.java
@@ -0,0 +1,13 @@
+public class Dsize{
+    public static void main(String[] args){
+
+        System.out.println("Size of byte:"+Byte.SIZE/8+"byte");
+        System.out.println("Size of byte:"+Character.SIZE/8+"byte");
+        System.out.println("Size of byte:"+Short.SIZE/8+"byte");
+        System.out.println("Size of byte:"+Long.SIZE/8+"byte");
+        System.out.println("Size of byte:"+Float.SIZE/8+"byte");
+        System.out.println("Size of byte:"+Double.SIZE/8+"byte");
+        System.out.println("Size of byte:"+Integer.SIZE/8+"byte");
+    }
+
+}
