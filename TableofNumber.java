47080490210f7be7d1e303409ddb75e068b8af9a
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-07-23 22:56:04 +0530

    Add files via upload

---
+++ TableofNumber.java
@@ -0,0 +1,12 @@
+public class TableofNumber{
+    public static void main(String[] args){
+        int i,j;
+        for( j=2;j<=5;j++){
+            for(i=1;i<=10;i++){
+                System.out.println(j*i);
+            }
+        }
+
+
+    }
+}
