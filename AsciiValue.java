c7645934d93f52355abbe7d631ad5174af9a50fa
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-07-23 22:08:50 +0530

    Add files via upload

---
+++ AsciiValue.java
@@ -0,0 +1,9 @@
+public class AsciiValue{
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+                System.out.println("ASCII value of  "  +i+ ":" +   (char)i);
+            }
+        }
+    }
+}
