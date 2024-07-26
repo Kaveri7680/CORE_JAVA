cfce3e1b87ad2dc9a732c4d348c9b181e6988586
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-07-26 22:52:43 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,21 @@
+//Table of given number in 4X4 matrix
+public class Table{
+    public static void main(String[] args){
+        int k=1;
+        int a=5;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4;j++){
+                System.out.print(a*k);
+                k++;
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
