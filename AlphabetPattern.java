7f2471aa5663dda7e64b74639a9b09e1b3e097dc
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-07-29 23:37:44 +0530

    Update AlphabetPattern.java

--- AlphabetPattern.java
+++ AlphabetPattern.java
@@ -1,27 +1,18 @@
 // Alphabet pattern
 public class AlphabetPattern{
     public static void main(String[] args) {
-        for(int i=1;i<=4;i++) {
-
-            if (i == 1) {
-                for (char j = 'a'; j <= 'd'; j++) {
-                    System.out.print((char) j);
+       
+            int count=0;
+                    for(char e='a';e<='p';e++){
+                        System.out.print(e);
+                        count++;
+                        if(count==4){
+                            System.out.println();
+                            count=0;
+                        }                        
+                    }
+                    System.out.println();
                 }
-            } else if (i == 2) {
-                for (char j = 'e'; j <= 'h'; j++) {
-                    System.out.print((char) j);
-                }
-            } else if (i == 3) {
-                for (char j = 'i'; j <= 'l'; j++) {
-                    System.out.print((char) j);
-                }
-            } else if (i == 4) {
-                for (char j = 'm'; j <= 'p'; j++) {
-                    System.out.print((char) j);
-                }
-            }
-
-            System.out.println();
+    
         }
-        }
-    }
+    
