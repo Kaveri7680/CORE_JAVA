90463fb9fbb99cca722dcc954869083ff4aed5aa
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-07-30 23:58:07 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,12 @@
+//Multiple of 8 in 1 to 100
+
+public class Multiple{
+    public static void main(String[] args){
+
+        for(int i=1;i<=100;i++){
+            if(i%8==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
