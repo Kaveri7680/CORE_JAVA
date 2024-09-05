68effeee31568e8b98c415ea86e590b2e3d2009b
Kaveri7680 <159430696+Kaveri7680@users.noreply.github.com>
2024-09-06 00:13:09 +0530

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -5,26 +5,31 @@ public class Animal {
     Animal(){
         System.out.println("In parent Class");
     }
-    public void sound(){
+   private void sound(){
         System.out.println("Animal Sound");
     }
 
 }
 
 class Dog extends Animal{
+
+    Dog(){
+        System.out.println("In child class");
+    }
     //Overriding
-    public void sound(){
+   public void sound(){
         System.out.println("Dog Bark");
     }
 }
 
 class Main{
     public static void main(String[] args) {
-        Animal animal=new Animal();
-         animal.sound();
+//        Animal animal=new Animal();
+//         animal.sound();
 
 
-         Animal d=new Dog();
+         Dog d=new Dog();  //  call goes to Parent and child  constructor because of  inheritance
+                              // here Dog has parent class so which is Animal there for  2 Constructor  print
          d.sound();
 
     }
